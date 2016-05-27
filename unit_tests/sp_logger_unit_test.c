283b1fb45e8dd70b451bd278765a90f31179f0fa
Matan Gizunterman <Matan.Gizunterman@arm.com>
2016-05-28 00:56:34 +0300

    dfds

--- unit_tests/sp_logger_unit_test.c
+++ unit_tests/sp_logger_unit_test.c
@@ -104,7 +104,7 @@ static bool identicalLines(const char* testFile,char* line){
 	char* tempLine = (char*)malloc(sizeof(char)*n);
 	FILE *fp;
 	fp = fopen(testFile, "r");
-	fgets(tempLine, n, fp);
+	tempLine = fgets(tempLine, n, fp);
 	bool a = strcmp(tempLine,line);
 	fclose(fp);
 	free(tempLine);
