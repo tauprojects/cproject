7e32a099ccddfb12689a6653b2c44fdbb7195b86
Matan Gizunterman <Matan.Gizunterman@arm.com>
2016-05-28 00:57:54 +0300

    afad

--- unit_tests/sp_logger_unit_test.c
+++ unit_tests/sp_logger_unit_test.c
@@ -104,7 +104,7 @@ static bool identicalLines(const char* testFile,char* line){
 	char* tempLine = (char*)malloc(sizeof(char)*n);
 	FILE *fp;
 	fp = fopen(testFile, "r");
-	tempLine = fgets(tempLine, n, fp);
+	char* temp  = fgets(tempLine, n, fp);
 	bool a = strcmp(tempLine,line);
 	fclose(fp);
 	free(tempLine);
