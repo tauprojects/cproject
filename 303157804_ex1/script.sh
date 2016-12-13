b7eadacfe4e35cab00cb4aaa925fc851527f389c
Matan Gizunterman <Matan.Gizunterman@arm.com>
2016-12-13 21:17:56 +0200

    aaa

---
+++ 303157804_ex1/script.sh
@@ -0,0 +1,15 @@
+mkdir 303157804
+cd 303157804
+mkdir temp
+$cat>temp/Matan
+$cat>temp/Gizuntermen
+$cat>temp/gizunterman
+cp temp/matan Gizuntermen
+cp temp/Gizuntermen matan
+rm temp/Matan
+rm temp/Gizuntermen
+mv temp/gizunterman gizunterman
+rmdir temp
+ls -l
+
+
